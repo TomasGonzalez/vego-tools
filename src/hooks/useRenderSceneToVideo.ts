5c74992ef4186088090ecd79fff44fee3af1c1bf
Tomas Gonzalez <tomasgp14@hotmail.com>
2022-11-30 20:17:13 -0400

    building canvas render

--- src/hooks/useRenderSceneToVideo.ts
+++ src/hooks/useRenderSceneToVideo.ts
@@ -1,7 +1,11 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 function useRenderScene() {
   const canvasRef = useRef<any>();
+
+  useEffect(() => {
+    console.log(canvasRef, 'this is canvas ref');
+  }, [canvasRef.current]);
 
   return { canvasRef };
 }
