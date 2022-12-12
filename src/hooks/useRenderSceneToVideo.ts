f659432b863e21873e3fe2440839e56e0fd3506f
Tomas Gonzalez <tomasgp14@hotmail.com>
2022-12-12 18:13:10 -0400

    refactored Scene to video

--- src/hooks/useRenderSceneToVideo.ts
+++ src/hooks/useRenderSceneToVideo.ts
@@ -1,12 +1,10 @@
 import { useRef, useEffect } from 'react';
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { createFFmpeg } from '@ffmpeg/ffmpeg';
 
 import useAnimationStore from '../stores/useAnimationStore';
 
 function useRenderScene() {
   const canvasRef = useRef<any>();
-  const mediaRecorderObject = useRef<any>();
-
   const setCurrentTime = useAnimationStore((store) => store.setCurrentTime);
   const mode = useAnimationStore((store) => store.mode);
   const setMode = useAnimationStore((store) => store.setMode);
@@ -20,118 +18,68 @@ function useRenderScene() {
           let frameCount = 0;
           await ffmpeg.load();
 
-          const onFinishCapturing = async () => {
-            console.log('is on stop running');
-
-            // Create an array of image data
-            console.log('loading images in ffmpeg');
-
-            // new Array(frameCount).forEach(async (_, i) => {
-            //   // console.log(urlBlob, 'this is image to render');
-            //   const num = `00${i}`.slice(-3);
-            //   try {
-            //     const written = ffmpeg.FS(
-            //       'writeFile',
-            //       `tmp.${num}.png`,
-            //       urlBlob
-            //     );
-            //     console.log(written);
-            //   } catch (err) {
-            //     console.log(err);
-            //   }
-            // });
-
-            console.log('running ffmpeg');
-
-            await ffmpeg.run(
-              '-framerate',
-              '30',
-              '-pattern_type',
-              'glob',
-              '-i',
-              '*.jpg',
-              '-c:a',
-              'copy',
-              '-shortest',
-              '-c:v',
-              'libx264',
-              '-pix_fmt',
-              'yuv420p',
-              '-vf',
-              'scale=1902:1584',
-              'out.mp4'
-            );
-
-            // TO_DO attach the images into a video Blob using FFMPEG
-            const data = ffmpeg.FS('readFile', 'out.mp4');
-
-            for (let i = 0; i < frameCount; i++) {
-              const num = `00${i}`.slice(-3);
-
-              ffmpeg.FS('unlink', `tmp.${num}.jpg`);
-            }
-
-            const downloadUrl = URL.createObjectURL(
-              new Blob([data.buffer], { type: 'video/mp4' })
-            );
-
-            setRenderBlob(downloadUrl);
-            console.log(downloadUrl);
-
-            setMode('render-results');
-          };
-
           // Start rendering by changing the canvas frame by frame and copturing it
-          const captureFrames = async () => {
-            setCurrentTime(0);
+          setCurrentTime(0);
+          while (
+            useAnimationStore.getState().currentTime <
+            useAnimationStore.getState().animationRecordTime
+          ) {
+            useAnimationStore.getState().setTimeNextFrame();
 
-            console.log('capture frame?');
+            await new Promise((resolve) => setTimeout(resolve, 100));
 
-            while (
-              useAnimationStore.getState().currentTime <
-              useAnimationStore.getState().animationRecordTime
-            ) {
-              useAnimationStore.getState().setTimeNextFrame();
+            const num = `00${frameCount++}`.slice(-3);
+            const currentFrame = canvasRef.current
+              .toDataURL('image/jpeg')
+              .replace(/^data:.+;base64,/, '');
 
-              console.log(
-                useAnimationStore.getState().currentTime,
-                'this is the current time'
+            try {
+              ffmpeg.FS(
+                'writeFile',
+                `tmp.${num}.jpg`,
+                Uint8Array.from(atob(currentFrame), (c: string) =>
+                  c.charCodeAt(0)
+                )
               );
-
-              await new Promise((resolve) => setTimeout(resolve, 100));
-
-              const num = `00${frameCount++}`.slice(-3);
-
-              const currentFrame = canvasRef.current
-                .toDataURL('image/jpeg')
-                .replace(/^data:.+;base64,/, '');
-
-              try {
-                ffmpeg.FS(
-                  'writeFile',
-                  `tmp.${num}.jpg`,
-                  Uint8Array.from(atob(currentFrame), (c: string) =>
-                    c.charCodeAt(0)
-                  )
-                );
-                console.log('Test');
-              } catch (err) {
-                console.log(err);
-              }
-
-              // renderChunks.push(canvasRef.current.toDataURL('image/png'));
-              // console.log(
-              //   'render chunks',
-              //   canvasRef.current.toDataURL('image/png', 1.0)
-              // );
-
-              // console.log('added frame');
+            } catch (err) {
+              console.log(err);
             }
+          }
 
-            onFinishCapturing();
-          };
+          await ffmpeg.run(
+            '-framerate',
+            '30',
+            '-pattern_type',
+            'glob',
+            '-i',
+            '*.jpg',
+            '-c:a',
+            'copy',
+            '-shortest',
+            '-c:v',
+            'libx264',
+            '-pix_fmt',
+            'yuv420p',
+            '-vf',
+            'scale=1902:1584',
+            'out.mp4'
+          );
 
-          captureFrames();
+          const data = ffmpeg.FS('readFile', 'out.mp4');
+
+          for (let i = 0; i < frameCount; i++) {
+            const num = `00${i}`.slice(-3);
+            ffmpeg.FS('unlink', `tmp.${num}.jpg`);
+          }
+
+          const downloadUrl = URL.createObjectURL(
+            new Blob([data.buffer], { type: 'video/mp4' })
+          );
+
+          setRenderBlob(downloadUrl);
+          console.log(downloadUrl);
+
+          setMode('render-results');
         }
       }
     })();
